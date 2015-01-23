ceb9e80833656f049e2cee3bcf148abb7f1ade79
oisinconnolly <orc215@lehigh.edu>
2015-01-23 16:55:09 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,23 @@
+// Oisin Connolly
+// Hello World Lab
+
+
+// Define a class
+public class HelloWorld{
+    
+//add main method
+public static void main(String[ ] args)  {
+
+//print the statement with a box around it
+    System.out.println("#####################");
+    System.out.println("#####################");    
+    System.out.println("#####################");
+    System.out.println("#                   #");
+    System.out.println("#  Hello World 2015 #");
+    System.out.println("#                   #");
+    System.out.println("#####################");
+    System.out.println("#####################");
+    System.out.println("#####################");
+    System.out.println("test");
+}
+}
