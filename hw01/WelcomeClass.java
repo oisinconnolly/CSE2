c76908874396ebf81c15c0ac8fe9c53e3e045205
oisinconnolly <orc215@lehigh.edu>
2015-01-25 21:50:01 +0000

    updated Welcome Class

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,21 @@
+
+
+// Define a class
+public class WelcomeClass{
+    
+//add main method
+public static void main(String[ ] args)  {
+
+//print the welcome message and autobiographical sentence
+    System.out.println("--------------");
+    System.out.println("|   WELCOME   |");    
+    System.out.println("--------------");
+    System.out.println("  ^   ^   ^   ^   ^   ^");
+    System.out.println(" / \\ / \\ / \\ / \\ / \\ / \\ ");
+    System.out.println("<-O---R---C---2---1---5->");
+    System.out.println(" \\ / \\ / \\ / \\ / \\ / \\ /");
+    System.out.println("  v   v   v   v   v   v  ");
+    System.out.println("I am a tri-citizen (Australia, Ireland, USA), ");
+    System.out.println("last semester senior who is an avid runner.");
+}
+}
